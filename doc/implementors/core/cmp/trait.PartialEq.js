a78471c8c08bea10ffc442d6cdc281e3f2d3a378
Robert Murray McMahon <Robert.Murray.McMahon@gmail.com>
2014-08-16 04:54:02 -0500

    Add documentation.

---
+++ doc/implementors/core/cmp/trait.PartialEq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['X11'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='type' href='X11/Connection_Error/type.ConnectionError.html' title='X11::Connection_Error::ConnectionError'>ConnectionError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/implementors/rand/trait.Rand.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['X11'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/rand/trait.Rand.html' title='rand::Rand'>Rand</a> for <a class='type' href='X11/Connection_Error/type.ConnectionError.html' title='X11::Connection_Error::ConnectionError'>ConnectionError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()

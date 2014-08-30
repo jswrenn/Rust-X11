3a12fe2cb4b002ce8c683875bb05438b591cdd1f
Robert Murray McMahon <Robert.Murray.McMahon@gmail.com>
2014-08-30 08:27:18 -0500

    Update documentation.

---
+++ doc/implementors/serialize/serialize/trait.Decodable.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['X11'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;__D: <a class='trait' href='http://doc.rust-lang.org/master/serialize/serialize/trait.Decoder.html' title='serialize::serialize::Decoder'>Decoder</a>&lt;__E&gt;, __E&gt; <a class='trait' href='http://doc.rust-lang.org/master/serialize/serialize/trait.Decodable.html' title='serialize::serialize::Decodable'>Decodable</a>&lt;__D, __E&gt; for <a class='struct' href='X11/window/struct.Window.html' title='X11::window::Window'>Window</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/implementors/serialize/serialize/trait.Encodable.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['X11'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;__S: <a class='trait' href='http://doc.rust-lang.org/master/serialize/serialize/trait.Encoder.html' title='serialize::serialize::Encoder'>Encoder</a>&lt;__E&gt;, __E&gt; <a class='trait' href='http://doc.rust-lang.org/master/serialize/serialize/trait.Encodable.html' title='serialize::serialize::Encodable'>Encodable</a>&lt;__S, __E&gt; for <a class='struct' href='X11/window/struct.Window.html' title='X11::window::Window'>Window</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()

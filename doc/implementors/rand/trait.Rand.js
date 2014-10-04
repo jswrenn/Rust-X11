f31632bea9555f9e207d4dca486bb530a936fec2
Robert Murray McMahon <Robert.Murray.McMahon@gmail.com>
2014-10-04 10:44:40 -0500

    Update documentation.

--- doc/implementors/rand/trait.Rand.js
+++ doc/implementors/rand/trait.Rand.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['X11'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/rand/trait.Rand.html' title='rand::Rand'>Rand</a> for <a class='type' href='X11/connection_error/type.ConnectionError.html' title='X11::connection_error::ConnectionError'>ConnectionError</a>",];
+implementors['X11'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/rand/trait.Rand.html' title='rand::Rand'>Rand</a> for <a class='enum' href='X11/connection_error/enum.ConnectionError.html' title='X11::connection_error::ConnectionError'>ConnectionError</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);

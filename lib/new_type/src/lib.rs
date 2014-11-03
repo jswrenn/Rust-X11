8ca07296c5439723e656ad2c0acd935943872134
Robert Murray McMahon <Robert.Murray.McMahon@gmail.com>
2014-11-03 04:57:57 -0600

    Continue refactoring “new_type”.

--- lib/new_type/src/lib.rs
+++ lib/new_type/src/lib.rs
@@ -9,7 +9,7 @@ use syntax::ast;
 use syntax::codemap::Span;
 use syntax::parse::token;
 use syntax::ext::base::{ExtCtxt, Decorator};
-use syntax::ext::quote::rt::{ExtParseUtils, ToTokens, ToSource};
+use syntax::ext::quote::rt::{ToTokens, ToSource};
 use syntax::ptr::P as Ptr;
 
 #[plugin_registrar]
@@ -62,16 +62,14 @@ fn expand_new_type(context: &mut ExtCtxt, span: Span,
         Some(str) => str,
         None => return
     };
-    let val_method_str = format!("as_{}", old_type_str);
-    let old_type_name = context.parse_tts(old_type_str);
-    let val_method_name = context.parse_tts(val_method_str.clone());
-    let ref_method_name = context.parse_tts(format!("{}_ref", val_method_str));
-    let val_method_comment_str = format!("Returns the underlying `{old}` in the `{new}`.", old=old_type_source, new=new_type_source);
-    let val_method_comment_tokens = val_method_comment_str.as_slice().to_tokens(context);
-    let val_method_comment = quote_tokens!(context, #[doc=$val_method_comment_tokens]);
-    let ref_method_comment_str = format!("Returns a reference to the underlying `{old}` in the `{new}`.", old=old_type_source, new=new_type_source);
-    let ref_method_comment_tokens = ref_method_comment_str.as_slice().to_tokens(context);
-    let ref_method_comment = quote_tokens!(context, #[doc=$ref_method_comment_tokens]);
+    let old_type_name = token::str_to_ident(old_type_str.as_slice());
+
+    let val_method_name = token::str_to_ident(format!("as_{}", old_type_str).as_slice());
+    let val_method_comment = make_comment(context, format!("Returns the underlying `{old}` in the `{new}`.", old=old_type_source, new=new_type_source));
+
+    let ref_method_name = token::str_to_ident(format!("as_{}_ref", old_type_str).as_slice());
+    let ref_method_comment = make_comment(context, format!("Returns a reference to the underlying `{old}` in the `{new}`.", old=old_type_source, new=new_type_source));
+
     let maybe_item_impl = quote_item!(context,
                                       ///Automatically generated methods from
                                       ///`new_type` syntax extension attribute.
@@ -85,7 +83,6 @@ fn expand_new_type(context: &mut ExtCtxt, span: Span,
                                           pub fn $val_method_name(&self) -> $old_type {
                                               self.$identifier
                                           }
-
                                           $ref_method_comment
                                           #[inline]
                                           pub fn $ref_method_name(&self) -> &$old_type {
@@ -96,71 +93,78 @@ fn expand_new_type(context: &mut ExtCtxt, span: Span,
     push(maybe_item_impl.expect("Entered unexpected branch in expansion of “new_type” attribute."))
 }
 
+fn make_comment(context: &mut ExtCtxt, comment: String) -> Vec<ast::TokenTree> {
+    let comment_tokens = comment.as_slice().to_tokens(context);
+    quote_tokens!(context, #[doc=$comment_tokens])
+}
+
 fn type_to_ident_str(context: &mut ExtCtxt, span: Span, type_: Ptr<ast::Ty>, format: fn(&[Ascii]) -> String) -> Option<String> {
+    use std::str::{MaybeOwned, Slice, Owned};
+
     let mut sub_type = &type_;
-    let mut result = String::new();
+    let mut type_names: Vec<MaybeOwned> = Vec::new();
     loop {
         match sub_type.node {
             ast::TyNil => {
-                result.push_str("Nil");
+                type_names.push(Slice("nil"));
                 break
             }
             ast::TyBot => {
-                result.push_str("Bot");
+                type_names.push(Slice("bot"));
                 break
             }
             ast::TyVec(ref ty) => {
-                result.push_str("Slice");
+                type_names.push(Slice("slice"));
                 sub_type = ty
             }
             ast::TyFixedLengthVec(ref ty, ref expr) => {
                 let expr_str = expr.to_source();
-                result.push_str("Array");
-                result.push_str(expr_str.as_slice());
+                type_names.push(Slice("array"));
+                type_names.push(Owned(expr_str));
                 sub_type = ty
             }
             ast::TyPtr(ref mut_ty) => {
                 let ast::MutTy {
-                    ty: ref ty,
-                    mutbl: mutbl
+                    ref ty,
+                    mutbl
                 } = *mut_ty;
-                result.push_str("Ptr");
-                if let ast::MutMutable = mutbl { result.push_str("Mut") }
+                type_names.push(Slice("ptr"));
+                if let ast::MutMutable = mutbl { type_names.push(Slice("mut")) }
                 sub_type = ty
             }
             ast::TyRptr(_, ref mut_ty) => {
                 let ast::MutTy {
-                    ty: ref ty,
-                    mutbl: mutbl
+                    ref ty,
+                    mutbl
                 } = *mut_ty;
-                result.push_str("Ref");
-                if let ast::MutMutable = mutbl { result.push_str("Mut") }
+                type_names.push(Slice("ref"));
+                if let ast::MutMutable = mutbl { type_names.push(Slice("mut")) }
                 sub_type = ty
             }
             ast::TyTup(ref vec) => {
-                result.push_str("Tup");
+                type_names.push(Slice("tup"));
                 for ty in vec.iter() {
                     if let Some(str) = type_to_ident_str(context, span, ty.clone(), format) {
-                        result.push_str(str.as_slice())
+                        type_names.push(Owned(str))
                     }
                 }
                 break
             }
             ast::TyPath(ref path, _, _) => {
+                if path.global { type_names.push(Slice("")) }
                 for segment in path.segments.iter() {
-                    result.push_str(segment.identifier.as_str())
+                    type_names.push(Owned(format(unsafe{segment.identifier.as_str().to_ascii_nocheck()})))
                 }
                 break
             }
             ast::TyParen(ref ty) => sub_type = ty,
             _ => {
                 context.span_err(span, "Unsupported type for automatic conversion to identifier.");
-                println!("{}", sub_type.node);
                 return None
             }
         }
     }
-    Some(format(unsafe{result.to_ascii_nocheck()}))
+    Some(format(unsafe{type_names.connect("_").to_ascii_nocheck()}))
 }
 
 //FIXME: Temporarily pasted version from other file.
